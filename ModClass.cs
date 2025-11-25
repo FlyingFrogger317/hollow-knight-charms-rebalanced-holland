fe57aa347b682e8540c99838eacff56eb1ed0d53
Anthony <198533390+FlyingFrogger317@users.noreply.github.com>
2025-11-24 18:37:41 -0800

    Mod setup

---
+++ ModClass.cs
@@ -0,0 +1,28 @@
+﻿using Modding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CharmsRebalanced
+{
+    public class CharmsRebalanced : Mod
+    {
+        internal static CharmsRebalanced Instance;
+        internal static string ModDisplayName = "CharmsRebalanced";
+        internal static string version = "1.0.0.0";
+        CharmsRebalanced() : base(ModDisplayName) { }
+        public override string GetVersion()
+        {
+            return version;
+        }
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            Log("Initializing");
+
+            Instance = this;
+
+            Log("Initialized");
+        }
+    }
+}
