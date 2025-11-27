51b7bdb6d447cf1bf93152dbb7d8bf3c2167d19d
Anthony <198533390+FlyingFrogger317@users.noreply.github.com>
2025-11-26 16:01:18 -0800

    IlHooks

--- ILHooks.cs
+++ ILHooks.cs
@@ -1,33 +1,40 @@
-using HarmonyLib;
 using MonoMod.Cil;
 using UnityEngine;
+using Modding;
+using Mono.Cecil.Cil;
+using MonoMod.RuntimeDetour.HookGen;
+
 // All IL hooks go here
-[HarmonyPatch(typeof(HeroController), "Move",new System.Type[] {typeof(float)})]
-public class SprintmasterMakeWorkInAir {
-    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator, ILContext context) {
-        ILCursor c = new ILCursor(context);
-        if (c.TryGotoNext(MoveType.Before,x=>x.MatchLdarg(0),x=>x.MatchLdfld("cState"),x=>x.MatchLdfld("onGround"),x=>x.MatchBrfalse(out ILLabel _))) {
-            c.RemoveRange(3);
-            c.Emit(OpCodes.Ldc_I4_1);
-            Log("Patched Sprintmaster to work in air if you also have Dashmaster.");
-        }
-        if (c.TryGotoNext(MoveType.Before,x=>x.MatchLdarg(0),x=>x.MatchLdfld("cState"),x=>x.MatchLdfld("onGround"),x=>x.MatchBrfalse(out ILLabel _))) {
-            c.RemoveRange(3);
-            c.Emit(OpCodes.Ldc_I4_1);
-            Log("Patched Sprintmaster to work in air.");
-        }
-        return c.Finish().ToArray();
-    }
-}
 namespace CharmsRebalanced.ILHooks
 {
-    
-    public static class ILHooks {
-        static private Harmony harmony;
-        static void Initialize() {
-            harmony = new Harmony("charmsrebalanced.ilhooks");
-            harmony.PatchAll();
-            Log("IL Hooks patched.");
+    public static class ILHooks
+    {
+        public static class SprintmasterMakeWorkInAir
+        {
+            public static void Enable()
+            {
+                IL.HeroController.Move += Patch;
+            }
+            public static void Disable()
+            {
+                IL.HeroController.Move -= Patch;
+            }
+            private static void Patch(ILContext il)
+            {
+                ILCursor c = new ILCursor(il);
+                if (c.TryGotoNext(MoveType.Before, x => x.MatchLdarg(0), x => x.MatchLdfld<HeroController>("cState"), x => x.MatchLdfld<HeroControllerStates>("onGround"), x => x.MatchBrfalse(out ILLabel _)))
+                {
+                    c.RemoveRange(3);
+                    c.Emit(OpCodes.Ldc_I4_1);
+                    CharmsRebalanced.Instance.Log("Patched Sprintmaster to work in air if you also have Dashmaster.");
+                }
+                if (c.TryGotoNext(MoveType.Before, x => x.MatchLdarg(0), x => x.MatchLdfld<HeroController>("cState"), x => x.MatchLdfld<HeroControllerStates>("onGround"), x => x.MatchBrfalse(out ILLabel _)))
+                {
+                    c.RemoveRange(3);
+                    c.Emit(OpCodes.Ldc_I4_1);
+                    CharmsRebalanced.Instance.Log("Patched Sprintmaster to work in air.");
+                }
+            }
         }
     }
 }
